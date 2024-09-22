ed992c77c0b3aed3004d4e68996ec630add4c26c
Jayesh Nagpurkar <jayeshnagpurkar2@gmail.com>
2024-09-22 16:57:17 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4000;
 if (process.env.NODE_ENV !== 'production') {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-  });
+  });  
 }
 
 module.exports = app;
